81dd7b01d122b1729c280d2c947dd4dc7ee3f98a
Mansur <lmk.dev@yandex.ru>
2024-10-20 16:25:19 +0300

    Update install script

--- run_once_before_install-01-apt-packages.sh
+++ run_once_before_install-01-apt-packages.sh
@@ -3,4 +3,4 @@
 sudo apt update
 sudo apt upgrade
 sudo apt install llvm clang build-essential pkg-config libssl-dev linux-headers-"$(uname -r)"
-sudo apt install curl zsh zip unzip p7zip-full git python3-pip python3-venv
+sudo apt install curl zsh zip unzip p7zip-full git python3-pip python3-venv tmux

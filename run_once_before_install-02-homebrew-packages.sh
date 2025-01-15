345fc47483025d278874ae075061200621ef8ec6
Mansur Lutfullin <lmk.dev@yandex.ru>
2025-01-15 09:47:48 +0300

    Create run_once_before_install-02-homebrew-packages.sh

---
+++ run_once_before_install-02-homebrew-packages.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/bash
+
+brew install \
+  ffind \
+  bat \
+  exa \
+  ripgrep \
+  zoxide \
+  git-delta \
+  tealdeer \
+  topgrade \
+  fzf \
+  lazygit \
+  neovim \
+  zinit \
+  oh-my-posh

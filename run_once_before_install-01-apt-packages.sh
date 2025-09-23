f139c1de5510369f9edc343c961c65e9480251be
Mansur <yaponomat@gmail.com>
2025-09-23 16:09:40 +0300

    Update install script

--- run_once_before_install-01-apt-packages.sh
+++ run_once_before_install-01-apt-packages.sh
@@ -1,6 +1,19 @@
 #!/usr/bin/bash
 
-sudo apt update
-sudo apt upgrade
-sudo apt install llvm clang build-essential pkg-config libssl-dev linux-headers-"$(uname -r)"
-sudo apt install curl zsh zip unzip p7zip-full git python3-pip python3-venv tmux
+source /etc/os-release
+NO_FORMAT="\033[0m"
+C_GREEN="\033[48;5;2m"
+
+ubuntu_install() {
+  sudo apt update
+  sudo apt upgrade
+  sudo apt install llvm clang build-essential pkg-config libssl-dev linux-headers-"$(uname -r)"
+  sudo apt install curl zsh zip unzip p7zip-full git python3-pip python3-venv tmux
+}
+
+case $ID in
+ubuntu)
+  echo -e "${C_GREEN} Find Ubuntu distr! Install packages ${NO_FORMAT}"
+  ubuntu_install
+  ;;
+esac

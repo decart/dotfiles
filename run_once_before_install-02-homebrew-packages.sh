086ce5ce98334f7ba89da2b2dc8733257dfdcbe6
Mansur Lutfullin <lmk.dev@yandex.ru>
2025-10-17 10:49:41 +0300

    Update run_once_before_install-02-homebrew-packages.sh

--- run_once_before_install-02-homebrew-packages.sh
+++ run_once_before_install-02-homebrew-packages.sh
@@ -1,18 +1,28 @@
 #!/usr/bin/bash
 
-brew install \
-  fd \
-  bat \
-  eza \
-  ripgrep \
-  zoxide \
-  git-delta \
-  tealdeer \
-  topgrade \
-  fzf \
-  lazygit \
-  neovim \
-  zinit \
-  yazi \
-  kanata \
-  oh-my-posh
+NO_FORMAT="\033[0m"
+C_RED="\033[48;5;203m"
+
+if brew --version > /dev/null 2>&1; then
+  brew install \
+    fd \
+    bat \
+    eza \
+    ripgrep \
+    zoxide \
+    git-delta \
+    tealdeer \
+    topgrade \
+    fzf \
+    lazygit \
+    neovim \
+    zinit \
+    yazi \
+    kanata \
+    oh-my-posh
+else
+  echo "${NO_RED} Homebrew is not installed. Skip! ${NO_FORMAT}";
+  exit 1
+fi
+
+

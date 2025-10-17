6f9b4635f6fcff317053628a00fb5cca6afcd378
Mansur Lutfullin <lmk.dev@yandex.ru>
2025-10-17 09:20:31 +0300

    Update run_once_before_install-03-configuration.sh

--- run_once_before_install-03-configuration.sh
+++ run_once_before_install-03-configuration.sh
@@ -1,3 +1,25 @@
 #!/usr/bin/bash
 
-gsettings set org.gnome.mutter experimental-features '["scale-monitor-framebuffer", "xwayland-native-scaling"]'
+source /etc/os-release
+NO_FORMAT="\033[0m"
+C_GREEN="\033[48;5;2m"
+
+ubuntu_install() {
+  gsettings set org.gnome.mutter experimental-features '["scale-monitor-framebuffer", "xwayland-native-scaling"]'
+}
+
+alt_install() {
+  gsettings set org.gnome.mutter experimental-features '["scale-monitor-framebuffer", "xwayland-native-scaling"]'
+}
+
+case $ID in
+ubuntu)
+  echo -e "${C_GREEN} Find Ubuntu distr! Update config ${NO_FORMAT}"
+  ubuntu_install
+  ;;
+
+altlinux)
+  echo -e "${C_GREEN} Find AltLinux distr! Update config ${NO_FORMAT}"
+  alt_install
+  ;;
+esac
